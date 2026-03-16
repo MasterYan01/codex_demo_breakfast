1f7857d1849c0a7a27af22d10021b44d87f07c03
Ethan <ethanyan@castec.com.tw>
2026-03-16 11:32:07 +0800

    提供 Mailchimp 表單資訊」} ??? wait can't;

--- docs/config.js
+++ docs/config.js
@@ -2,9 +2,17 @@
   apiBase: 'https://la-miu-backend.onrender.com',
   googleReviewUrl: '',
   newsletter: {
-    action: '',
+    action: 'https://gmail.us5.list-manage.com/subscribe/post',
     method: 'post',
-    emailField: 'EMAIL',
-    hiddenFields: {}
+    emailField: 'MERGE0',
+    hiddenFields: {
+      u: '1f2e9b6a8945af20d46687e4e',
+      id: '1db3bb1f1e',
+      ht: 'fd330d87e135c66d8be55b99f544f1e457f7efb5:MTc3MzYzMTMwNS4yOTU2',
+      mc_signupsource: 'hosted',
+      b_name: '',
+      b_email: '',
+      b_comment: ''
+    }
   }
 };
